fba098a991c0cea761b2973f80ad669fe569904d
estewart413 <dane.stewart@selu.edu>
2021-05-05 02:09:40 -0500

    Added Angular Material
    Test Heroku API pulls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
   public localApiResponse: Object;
   constructor(private http: HttpClient){
     this.localApiResponse = '';
-    this.http.get('/').subscribe(
+    this.http.get('/quizzes').subscribe(
       apiResponse => {
         this.localApiResponse = JSON.stringify(apiResponse);
     });
